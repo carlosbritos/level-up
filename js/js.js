c5e4f7c78d8a7cf063269577685e70c23ab5690e
Carlos Britos <carlosbritos_98@hotmail.com>
2019-03-31 23:05:04 -0300

    slider valores

---
+++ js/js.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    center:true,
+    dots:false,
+    autoplay:true,
+    items:1,
+
+
+
+    /* responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        800:{
+            items:3
+        }
+    } */
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
