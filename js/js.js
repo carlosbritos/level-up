2c64a92ce6a1b72e3f9faa887632d1ff7d21ca68
carlos.britos <carlosbritos1114@gmail.com>
2019-04-04 17:18:46 -0300

    nuevoo

--- js/js.js
+++ js/js.js
@@ -12,7 +12,7 @@ var funciones = {
 
 $(document).ready(function(){
 
-  $('.owl-carousel').owlCarousel({
+  $('.slideValores').owlCarousel({
     
     loop:true,
     margin:0,
@@ -37,6 +37,31 @@ $(document).ready(function(){
     } */
   })
 
+
+  $('.letras').owlCarousel({
+    
+    loop:true,
+    margin:0,
+    nav:false,
+    center:true,
+    dots:false,
+    autoplay:false,
+    items:30,
+
+
+
+    /* responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        800:{
+            items:3
+        }
+    } */
+  })
 
 
   $('.colorClick').on( "click", function(){
